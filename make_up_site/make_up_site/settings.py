1cefe247384808619f01c1b9f5de5b0a174f9807
Vlada <zikzak200015@gmail.com>
2020-01-16 22:46:01 +0200

    changed settings.py

--- make_up_site/make_up_site/settings.py
+++ make_up_site/make_up_site/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'make_up_site.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['make_up_site'],
+        'DIRS': [os.path.join(BASE_DIR, 'make_up_site\\templates'), os.path.join(BASE_DIR, 'makeup\\templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
